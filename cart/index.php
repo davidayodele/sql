eb71af2e60155fda256ee4014609d3647d2d15c4
davidayodele <starcanyonschool@gmail.com>
2020-11-30 08:04:43 -0700

    edited index to show getData output

--- cart/index.php
+++ cart/index.php
@@ -71,8 +71,8 @@ echo "Connected still!";
 <div class="container">
         <div class="row text-center py-5">
             <?php
-                $result = $database->getData();
                 echo "Hiyah";
+                $result = $database->getData();                
                 echo $result;
                 while ($row = mysqli_fetch_assoc($result)){
                     component($row['product_name'], $row['price'], $row['img'], $row['id']);
