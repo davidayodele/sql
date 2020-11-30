6f90ccf4dba11407a5ff4c223f1ba2d020a3b3e2
davidayodele <starcanyonschool@gmail.com>
2020-11-30 08:29:00 -0700

    edited index to show getData output

--- cart/index.php
+++ cart/index.php
@@ -69,7 +69,7 @@ echo "Connected still!";
 <h1> Hi there </h1>
 
 <?php 
-if(file_exists($_SESSION['ROOT_PATH'] . '/php/header.php'))
+if(file_exists($_SESSION['ROOT_PATH'] . '/php/component.php'))
 {
  echo 'OK';
 } else {
