c5a0df9d36716b95703ca7578ab0f4546f860dbf
davidayodele <starcanyonschool@gmail.com>
2020-11-30 11:01:59 -0700

    edited db setup

--- cart/php/CreateDb.php
+++ cart/php/CreateDb.php
@@ -64,7 +64,7 @@ class CreateDb
                     CREATE TABLE IF NOT EXISTS $tablename2
                             (email VARCHAR(255) NOT NULL DEFAULT 'N/A',
                              order_id INT(11) NOT NULL FOREIGN KEY REFERENCES order(order_id),
-                             DOB DATE NOT NULL,
+                             DOB DATE,
                              phone char(50),
                              customer_name VARCHAR (255)
                             );
