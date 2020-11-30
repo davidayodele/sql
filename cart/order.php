88702cbb1131bcf3c824bfcf7647ffbcdcac6bc1
davidayodele <starcanyonschool@gmail.com>
2020-11-30 15:41:40 -0700

    edited index

--- cart/order.php
+++ cart/order.php
@@ -7,7 +7,7 @@ require ("php/component.php");
 //$db_conn = mysqli_connect($servername, $username, $password, $dbname); // must be in order (host, user, pass, db)
 $database = new CreateDb("starcany_loquodb", "orders");
 
-//include("php/header.php"); // do not use require_once()
+include("php/header.php"); // do not use require_once()
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is coming from a form
 	$name = $_POST["customer_name"]; //set PHP variables like this so we can use them anywhere in code below
@@ -38,3 +38,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is coming from a form
     echo "<br><br><a href='index.php' class='btn'>Return</a>";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<br>
+<br>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
