0574c7e85d9aea49a94d0d9e4653dc9fd46e4d21
davidayodele <starcanyonschool@gmail.com>
2020-11-30 20:45:15 -0700

    edited store home

--- cart/index.php
+++ cart/index.php
@@ -97,6 +97,8 @@ include("php/header.php"); // do not use require_once()
             ?>
         </div>
 </div>
+<center>
+
 <br>
 <br>
 <br>
@@ -154,7 +156,7 @@ Date of birth: <textarea name="user_text"></textarea><br />
 <input type="submit" value="Submit" />
 </form>
 -->
-
+</center>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
